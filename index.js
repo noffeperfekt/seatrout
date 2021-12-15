f609098b966715dfbbe2f16c4f61264f733ca70b
kristofer <kristofer.mansson@gmail.com>
2021-12-15 11:16:36 +0100

    Added names

---
+++ index.js
@@ -0,0 +1,19 @@
+export const name = {
+  danish: "havørred",
+  english: "seatrout",
+  english_full: "searun brown trout",
+  estonian: "meriforell",
+  finnish: "meritaimen",
+  frensh: "truite de mer",
+  german: "meerforelle",
+  icelandic: "sjóbirtingur",
+  latin: "salmo trutta",
+  latin_full: "salmo trutta morpha trutta",
+  latvian: "taimiņa",
+  lithuanian: "šlakiai",
+  netherland: "zeeforel",
+  norwegian: "sjøørret",
+  polish: "troć wędrowna",
+  russian: "морская форель",
+  swedish: "havsöring"
+};
